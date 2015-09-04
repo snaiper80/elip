c58d5e019c8a3ac52389f77e11887f993593af59
Lee GiTack <snaiper80@gmail.com>
2015-09-04 17:59:30 +0900

    Accept  IPv4 string and IP tuple of lookup/1

--- src/elip.erl
+++ src/elip.erl
@@ -34,9 +34,13 @@ start() ->
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
--spec lookup(string()) -> {ok, {found, string()} | not_found}.
-lookup(IpStr) ->
-    gen_server:call(?MODULE, {lookup, IpStr}).
+-spec lookup(string() | {non_neg_integer(), non_neg_integer(), non_neg_integer(), non_neg_integer()})
+        -> {ok, {found, string()} | not_found}.
+lookup(IpStr) when is_list(IpStr) ->
+    {ok, Addr} = inet_parse:address(IpStr),
+    gen_server:call(?MODULE, {lookup, Addr});
+lookup(Ip) when is_tuple(Ip), size(Ip) =:= 4 ->
+    gen_server:call(?MODULE, {lookup, Ip}).
 
 %% ===================================================================
 %% gen_server Function Definitions
@@ -48,11 +52,11 @@ init(_Args) ->
         cidr_map = load_cidr_map(default_config_path())
     }}.
 
-handle_call({lookup, IpStr}, _From, #state { cidr_map = CidrMap } = State) ->
-    {ok, Addr} = inet_parse:address(IpStr),
-    {reply,
-        {ok, cidr_map:find_network(Addr, CidrMap)},
-     State};
+handle_call({lookup, {I0, I1, I2, I3} = Ip}, _, State) when is_integer(I0), I0 >= 0, I0 < 256,
+                                                            is_integer(I1), I1 >= 0, I1 < 256,
+                                                            is_integer(I2), I2 >= 0, I2 < 256,
+                                                            is_integer(I3), I3 >= 0, I3 < 256 ->
+    {reply, {ok, cidr_map:find_network(Ip, State#state.cidr_map)}, State};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
